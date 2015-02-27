215d0ecb1f797fa634d09d194530b0200fb33197
Silas Barta <sbarta@gmail.com>
2015-02-26 22:44:47 -0800

    Simple copying of branch.

--- git-upstage.sh
+++ git-upstage.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-git branch | awk '/^\*/{print $2}'
+# TODO allow command-line input of branch to copy
+# Assumes you're on the branch you want to copy.
+CURRENT_BRANCH=`git branch | awk '/^\*/{print $2}'`
+NEW_BRANCH_NAME=quick-${CURRENT_BRANCH}
+
+# Copy branch
+git branch $NEW_BRANCH_NAME $CURRENT_BRANCH
+# Check that branch out
+git checkout $NEW_BRANCH_NAME
+# Squash to one commit
+# TODO
+# Amend message
+# TODO
+# Move name back
+# TODO
+# Push to remote
+# TODO
